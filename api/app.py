697eb8ea87ff5f9a4f45b17fb5e546a007a94167
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-09 11:19:25 -0500

    Remove documents endpoints (#106)
    * Remove documents endpoints
    
    * remove the count endpoint too

--- api/app.py
+++ api/app.py
@@ -20,7 +20,6 @@ from src.attribution.attribution_queue_service import (
 )
 from src.cache.redis import create_connection_pool
 from src.config import get_config
-from src.documents import documents_router
 from src.health import health_router
 from src.infini_gram_exception_handler import infini_gram_engine_exception_handler
 from src.infinigram import infinigram_router
@@ -52,7 +51,6 @@ add_exception_handler(
 
 app.include_router(health_router)
 app.include_router(router=infinigram_router)
-app.include_router(router=documents_router)
 app.include_router(router=attribution_router)
 
 tracer_provider = TracerProvider()
--- api/src/infinigram/infinigram_router.py
+++ api/src/infinigram/infinigram_router.py
@@ -1,12 +1,5 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Query
+from fastapi import APIRouter
 from infini_gram_processor.index_mappings import AvailableInfiniGramIndexId
-from infini_gram_processor.models import (
-    InfiniGramCountResponse,
-)
-
-from src.infinigram.infini_gram_dependency import InfiniGramProcessorDependency
 
 infinigram_router = APIRouter()
 
@@ -14,13 +7,3 @@ infinigram_router = APIRouter()
 @infinigram_router.get(path="/indexes")
 def get_available_indexes() -> list[AvailableInfiniGramIndexId]:
     return [index for index in AvailableInfiniGramIndexId]
-
-
-@infinigram_router.get("/{index}/count")
-def count(
-    query: Annotated[str, Query(examples=["Seattle"])],
-    infini_gram_processor: InfiniGramProcessorDependency,
-) -> InfiniGramCountResponse:
-    result = infini_gram_processor.count_n_gram(query=query)
-
-    return result
